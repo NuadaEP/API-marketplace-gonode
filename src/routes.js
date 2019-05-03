0809a88635861e1637c48a6e80b0d8885ae935a4
Bruno Cardoso <bruno@liveonsolutions.com>
2019-05-02 23:58:47 -0300

    Prepare register user and encrypting user password

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,14 @@ const express = require('express')
 
 const routes = express.Router()
 
+// Require controllers
+const UserController = require('./app/controllers/UserController')
+
+// Uses
+
+// Routes
+routes.post('/users', UserController.store)
+
 routes.get('/', (req, res) => res.send('Everything is working'))
 
 module.exports = routes
--- src/server.js
+++ src/server.js
@@ -1,15 +1,27 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const databaseConfig = require('./config/database')
 
 class App {
   constructor () {
     this.express = express()
     this.isDev = process.env.NODE_ENV !== 'production'
 
+    this.database()
     this.middwares()
     this.routes()
   }
 
-  middwares () {}
+  database () {
+    mongoose.connect(databaseConfig.uri, {
+      useCreateIndex: true,
+      useNewUrlParser: true
+    })
+  }
+
+  middwares () {
+    this.express.use(express.json())
+  }
 
   routes () {
     this.express.use(require('./routes'))
