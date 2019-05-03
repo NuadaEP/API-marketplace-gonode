f177f1cc02a0c66fcf3ff3288aa25932f557e6f0
Bruno Cardoso <bruno@liveonsolutions.com>
2019-05-03 11:16:51 -0300

    Adding JWT and generating API token

--- src/routes.js
+++ src/routes.js
@@ -4,11 +4,13 @@ const routes = express.Router()
 
 // Require controllers
 const UserController = require('./app/controllers/UserController')
+const SessionController = require('./app/controllers/SessionController')
 
 // Uses
 
 // Routes
 routes.post('/users', UserController.store)
+routes.post('/sessions', SessionController.store)
 
 routes.get('/', (req, res) => res.send('Everything is working'))
 
