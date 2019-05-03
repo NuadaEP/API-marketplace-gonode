a7906424c54f3f89c60b762e6e56db54ace10f79
Bruno Cardoso <bruno@liveonsolutions.com>
2019-05-03 12:01:01 -0300

    generating access token

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,9 @@ const express = require('express')
 
 const routes = express.Router()
 
+// Middlewares
+const AuthMiddlewares = require('./app/middlewares/auth')
+
 // Require controllers
 const UserController = require('./app/controllers/UserController')
 const SessionController = require('./app/controllers/SessionController')
@@ -11,6 +14,8 @@ const SessionController = require('./app/controllers/SessionController')
 // Routes
 routes.post('/users', UserController.store)
 routes.post('/sessions', SessionController.store)
+
+routes.get('/teste', AuthMiddlewares, (req, res) => res.json({ ok: true }))
 
 routes.get('/', (req, res) => res.send('Everything is working'))
 
