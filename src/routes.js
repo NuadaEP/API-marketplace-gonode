ead66bfac307484558d32df08587c4d557c4d30e
Bruno Cardoso <bruno@liveonsolutions.com>
2019-05-07 23:43:31 -0300

    installing express-validate and apllying the validator rules as middleware into routes

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const validate = require('express-validation')
 
 const routes = express.Router()
 
@@ -8,21 +9,36 @@ const AuthMiddlewares = require('./app/middlewares/auth')
 // Require controllers
 const controllers = require('./app/controllers')
 
+// Require validator rules
+const validators = require('./app/validators')
+
 // Routes
-routes.post('/users', controllers.UserController.store)
-routes.post('/sessions', controllers.SessionController.store)
+routes.post(
+  '/users',
+  validate(validators.User),
+  controllers.UserController.store
+)
+routes.post(
+  '/sessions',
+  validate(validators.Session),
+  controllers.SessionController.store
+)
 
 routes.use(AuthMiddlewares)
 
 // Ads rotues
 routes.get('/ads', controllers.AdController.index)
 routes.get('/ads/:id', controllers.AdController.show)
-routes.post('/ads', controllers.AdController.store)
-routes.put('/ads/:id', controllers.AdController.update)
+routes.post('/ads', validate(validators.Ad), controllers.AdController.store)
+routes.put('/ads/:id', validate(validators.Ad), controllers.AdController.update)
 routes.delete('/ads/:id', controllers.AdController.destroy)
 
 // Purchase routes
-routes.post('/purchase', controllers.PurchaseController.store)
+routes.post(
+  '/purchase',
+  validate(validators.Purchase),
+  controllers.PurchaseController.store
+)
 
 routes.get('/', (req, res) => res.send('Everything is working'))
 
